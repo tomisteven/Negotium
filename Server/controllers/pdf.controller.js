decfaa88a1fc2dfd3a39deab23e5d5517e18fd75
Tomas Steven Desktop <tomssteven22@gmail.com>
2023-01-08 17:51:37 -0300

    UNLINK fs delete image

--- Server/controllers/pdf.controller.js
+++ Server/controllers/pdf.controller.js
@@ -1,11 +1,12 @@
 import User from "../models/user";
-import { getFiles } from "../utils/images";
+import { getFiles, getPDF } from "../utils/images";
+
+import fs from "fs";
 /* nombre: String,
         tipo: String,
         servicio: String,
         url: String,
         fecha: Date */
-
 
 const createPdf = async (req, res) => {
     const { user_id } = req.user;
@@ -13,7 +14,7 @@ const createPdf = async (req, res) => {
     const response = await User.findById(user_id);
     const pdfs = response.pdfs;
         if(req.files){
-            const newUrl = getFiles(req.files.url)
+            const newUrl = getFiles(req.files.url, nombre)
             const newPdf = {
                 nombre: nombre,
                 servicio : servicio,
@@ -36,6 +37,19 @@ const getPDFs = async (req, res) => {
     response ? res.status(200).json(pdfs) : res.status(404).json({message: "No es un id Valido"});
 }
 
+const get = async (req, res) => {
+    const { user_id } = req.user;
+    const id = req.params.id;
+    const response = await User.findById(user_id)
+    const pdfs = response.pdfs
+    const pdf = pdfs.filter((pdf) => {
+         return pdf._id == id;
+    }
+    );
+
+    response ? res.status(200).json(pdf) : res.status(404).json({message: "No es un id Valido"});
+}
+
 const deletePdf = async (req, res) => {
     const { user_id } = req.user;
     const id = req.params.id;
@@ -46,12 +60,29 @@ const deletePdf = async (req, res) => {
     });
     response.pdfs = newPdfs;
     const result = await response.save();
-    result ? res.status(200).json(result) : res.status(404).json({message: "No es un id Valido"});
+
+    //eliminar del servidor
+    const pdf = pdfs.filter((pdf) => {
+        return pdf._id == id;
+    });
+    const url = pdf[0].url;
+    const path = `./uploads/${url}`;
+    fs.unlink(path, (err) => {
+        if(err){
+            console.log(err)
+        }else{
+            console.log("Archivo eliminado")
+        }
+    })
+
+
+
+
+    result ? res.status(200).json(result.pdfs) : res.status(404).json({message: "No es un id Valido"});
 
 }
 
 
 export {
-    createPdf, getPDFs, deletePdf
-
+    createPdf, getPDFs, deletePdf, get
 }
--- Server/utils/images.js
+++ Server/utils/images.js
@@ -1,7 +1,16 @@
-export const getFiles = (file) => {
+export const getFiles = (file, nombre) => {
     const filePath = file.path;
     const fileSplit = filePath.split("\\");
     const fileComplete = fileSplit[1] + "/"+ fileSplit[2];
 
     return fileComplete;
 }
+
+export const getPDF = (file, nombre) => {
+    const filePath = file.path;
+    const fileSplit = filePath.split("\\");
+    const fileComplete = fileSplit[1] + "/"+ nombre + ".pdf";
+
+    return fileComplete;
+}
+
