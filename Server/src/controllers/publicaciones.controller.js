fde8cffcd86c81ffc549c027927eccf93515b040
Tomas Steven Desktop <tomssteven22@gmail.com>
2023-01-23 19:46:59 -0300

    upload v9

--- Server/src/controllers/publicaciones.controller.js
+++ Server/src/controllers/publicaciones.controller.js
@@ -64,35 +64,49 @@ const createPublicacionAndImage = async(req, res) => {
         descripcion,
         subtitulo,
         imagen: "",
+        localUrl: ""
     }
-    //console.log(req.files.imagen);
     if(req.files.imagen){
-         try{
-            cloudinary.v2.uploader.upload(req.files.imagen.path, { public_id: titulo }, function(error, result) {
-                if(result){
-                    newPublicacion.imagen = result.url;
-                    user.publicaciones.push(newPublicacion);
-                    user.save((err, publicacionStored) => {
-                        if(err){
-                            res.status(500).send({message: "Error del servidor"})
-                        }else{
-                            if(publicacionStored){
-                                res.status(200).send({message: "Imagen subida correctamente y eliminada local", arr : publicacionStored.publicaciones})
-                            }
-                            else{
-                                res.status(404).send({message: "No se ha encontrado la publicacion"})
-                            }
-                }
-                if(error){
-                    res.status(500).send({message: "Error del servidor", error})
-                }
-            });
-        }})
-         }
-            catch(error){
+        const local = getFiles(req.files.imagen);
+        console.log(local);
+         cloudinary.v2.uploader.upload(req.files.imagen.path, { public_id: titulo }, function(error, result) {
+            if(result){
+                console.log(local);
+                newPublicacion.localUrl = local;
+                newPublicacion.imagen = result.url;
+                user.publicaciones.push(newPublicacion);
+                user.save((err, publicacionStored) => {
+                    if(err){
+                        res.status(500).send({message: "Error del servidor"})
+                    }else{
+                        if(publicacionStored){
+                            res.status(200).send({message: "Imagen subida correctamente y eliminada local",arr : publicacionStored.publicaciones})
+                            //eliminamos del servidor local
+                            /* const news = user.publicaciones.find((i) => {
+                                return i.localUrl == local;
+                            }) */
+                            /* const url = news.localUrl;
+                            const path = `./uploads/${url}`; */
+                            /* fs.unlink(path, (err) => {
+                                if(err){
+                                    console.log(err)
+                                 }else{
+
+                                }
+                            })
+                            //console.log(news); */
+                        }
+                        else{
+                            res.status(404).send({message: "No se ha encontrado la publicacion"})
+                        }
+            }
+            if(error){
                 res.status(500).send({message: "Error del servidor", error})
             }
-}
+        });
+    }
+
+}) }
 }
 
 const getNews = async (req,res) => {
